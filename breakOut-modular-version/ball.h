b625be6d0ba89b15e038e8921ee15bb3e57def82
Raul pallares <rpallares@miners.utep.edu>
2023-04-18 18:13:53 -0600

    for ball file

--- breakOut-modular-version/ball.h
+++ breakOut-modular-version/ball.h
@@ -6,11 +6,9 @@
 
 // row = zero element and column  = first element //
 // start at center screen//
-extern int ball_position[2]; 
+extern int ball_position[2], next_position[2]; 
 
-extern int next_position[2]; 
-
-extern int ball_velocity[2]; // x = columns, y = row direction
+extern int ball_velocity[2];
 
 extern int colLimits[2], rowLimits[2];
 
